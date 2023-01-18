4ab150cd8e1156897c68c9e849553c37eec975d8
Eric Hu <huericnan@gmail.com>
2023-01-17 21:48:55 -0500

    update

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
-import logging
-from pythonjsonlogger import jsonlogger
+from google.cloud import logging
 from datetime import datetime
 import time
 
 
-class CustomJsonFormatter(jsonlogger.JsonFormatter):
-    def add_fields(self, log_record, record, message_dict):
-        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
-        if not log_record.get("timestamp"):
-            now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            log_record["timestamp"] = now
-        if log_record.get("level"):
-            log_record["level"] = log_record["level"].upper()
-        else:
-            log_record["level"] = record.levelname
-
-
-logger = logging.getLogger()
-
-logger.setLevel(logging.DEBUG)
-
-logHandler = logging.StreamHandler()
-formatter = CustomJsonFormatter("%(timestamp)s %(level)s %(name)s %(message)s")
-logHandler.setFormatter(formatter)
-logger.addHandler(logHandler)
+logging_client = logging.Client()
+logger = logging_client.logger("demo-log")
 
 while True:
-    logger.debug("Request made.", extra={"key": "value", "num": 12})
+    logger.log_struct(
+        {"message": "Request made.", "key": "value", "num": 12}, severity="DEBUG"
+    )
     time.sleep(1)
-    logger.info("A user has logged in.", extra={"username": "testuser"})
+    logger.log_struct(
+        {"message": "A user has logged in.", "username": "testuser"}, severity="INFO"
+    )
     time.sleep(1)
-    logger.warning("Low disk space.", extra={"size": 25})
+    logger.log_struct({"message": "Low disk space.", "size": 25}, severity="WARNING")
     time.sleep(1)
-    logger.error("Error connecting to database.", extra={"dbname": "maindb"})
+    logger.log_struct(
+        {"message": "Error connecting to database.", "dbname": "maindb"},
+        severity="ERROR",
+    )
     time.sleep(1)
-    logger.critical("Application crashed.", extra={"code": 12345})
+    logger.log_struct(
+        {"message": "Application crashed.", "code": 12345}, severity="CRITICAL"
+    )
     time.sleep(1)
