10d993f00fac3e4f11513ea0de790f73f5fd93d2
Eric Hu <huericnan@gmail.com>
2023-01-23 18:30:14 -0500

    update

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ logger = logging_client.logger("demo-log")
 
 @app.route("/")
 def hello_world():
-    logger.log_struct(
-        {"message": "Request made.", "key": "value", "num": random.randint(0,50)}, severity="DEBUG"
-    )
-    logger.log_struct(
-        {"message": "A user has logged in.", "username": "testuser"}, severity="INFO"
-    )
-    logger.log_struct({"message": "Low disk space.", "size": random.randint(0,100)}, severity="WARNING")
-    logger.log_struct(
-        {"message": "Error connecting to database.", "dbname": "maindb"},
-        severity="ERROR",
-    )
-    logger.log_struct(
-        {"message": "Application crashed.", "code": random.randint(1111,9999)}, severity="CRITICAL"
-    )
+    for i in range(20):
+        logger.log_struct(
+            {"message": "Request made.", "key": "value", "num": random.randint(0,50)}, severity="DEBUG"
+        )
+        logger.log_struct(
+            {"message": "A user has logged in.", "username": "testuser"}, severity="INFO"
+        )
+        logger.log_struct({"message": "Low disk space.", "size": random.randint(0,100)}, severity="WARNING")
+        logger.log_struct(
+            {"message": "Error connecting to database.", "dbname": "maindb"},
+            severity="ERROR",
+        )
+        logger.log_struct(
+            {"message": "Application crashed.", "code": random.randint(1111,9999)}, severity="CRITICAL"
+        )
     return render_template("index.html")
